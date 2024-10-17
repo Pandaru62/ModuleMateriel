f3423ca19c730361bdd7c41b2ffd42b8cbbb2b3a
Loris Pandaru <loris.buchelet@gmail.com>
2024-10-17 18:30:35 +0200

    webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development', // Change to 'production' for production
-  entry: './src/index.js', // Make sure this path exists or update it
+  mode: 'production', 
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
